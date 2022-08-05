059de61b2b2d84ffe1aa0f62bde7a88776978d0d
james <sj2@penrose.us>
2022-08-04 17:40:07 -0700

    clients send ship data

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
+let myUsername = undefined;
 window.onload = () => {
 
     const landingPage = document.querySelector(".landingPage")
     const startButton = document.querySelector(".startButton")
     const gamePage = document.querySelector(".gamePage")
 
-    startButton.onclick = () => {
-        gamePage.style.display = "block"
-        landingPage.remove()
-        startGame()
-    }
 
     let prevTime = 0
 
@@ -43,37 +39,32 @@ window.onload = () => {
         }
 
         gameLoop(10, (dt) => {
-            console.log("mousex: " + mouseX)
-            if (mouseX != 0 && mouseY != 0) {
+                if (mouseX != 0 && mouseY != 0) {
 
-                ships.forEach((ship) => {
-                    ship.persue({ x: mouseX, y: mouseY }, dt)
-                })
-            }
-        })
+                    const shipPositions = []
+                    ships.forEach((ship) => {
+                        ship.persue({ x: mouseX, y: mouseY }, dt)
+                        const currentPosition = ship.element.getElementPosition()
+                            // console.log("ship position: " + currentPosition.x + ", " + currentPosition.y)
+                        shipPositions.push(currentPosition)
+                    })
 
-        document.onkeydown = function(event) {
+                    // console.log("swarm positions: ", shipPositions)
+                    const dataToSend = { username: myUsername, target: { x: mouseX, y: mouseY }, swarmPositions: shipPositions }
+                        // console.log("data: ", dataToSend)
+                    holler.appInstance.notifyClients(JSON.stringify(dataToSend))
 
-            switch (event.key) {
-                // case "w":
-                //     moveElement(ship, 0, -5)
-                //     break;
-                // case "a":
-                //     moveElement(ship, -5, 0)
-                //     break;
-                // case "s":
-                //     moveElement(ship, 0, 5)
-                //     break;
-                // case "d":
-                //     moveElement(ship, 5, 0)
-                //     break;
-                case " ":
-                    littleSound.play();
-                    break;
-                case "q":
-                    endGameLoop = true
-            }
-        };
+                    otherPlayers.forEach((playerData) => {
+                        const target = playerData.target
+
+                        playerData.swarmPositions.forEach((position) => {
+                            // const ship = new Ship()
+                            // ship.persue(target, dt)
+                        })
+                    })
+                }
+            })
+            // otherPlayers
 
         document.body.onmousemove = function(event) {
             mouseX += event.movementX
@@ -98,16 +89,85 @@ window.onload = () => {
             if (!endGameLoop)
                 setTimeout(() => { gameLoop(loopTime, action) }, loopTime)
         }
+        // document.onkeydown = function(event) {
+
+        //     switch (event.key) {
+        //         // case "w":
+        //         //     moveElement(ship, 0, -5)
+        //         //     break;
+        //         // case "a":
+        //         //     moveElement(ship, -5, 0)
+        //         //     break;
+        //         // case "s":
+        //         //     moveElement(ship, 0, 5)
+        //         //     break;
+        //         // case "d":
+        //         //     moveElement(ship, 5, 0)
+        //         //     break;
+        //         case " ":
+        //             littleSound.play();
+        //             break;
+        //         case "q":
+        //             endGameLoop = true
+        //     }
+        // };
     }
 
+    function isJson(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+    function isntMe(username) {
+        return username != myUsername
+    }
+
+    // function contains(list, item) {
+    //     return list.find((currentItem) => {
+    //         currentItem == item
+    //     }) != undefined
+    // }
+
+    const otherPlayers = []
+
+    holler.onLoad(() => {
+        holler.me((user) => {
+            myUsername = user.name
+            holler.appInstance.notifyClients("Hey i'm " + user.name)
+
+
+            holler.onClientEvent((event) => {
+                console.log("Recived: " + event)
+
+                const dataRecived = JSON.parse(event)
+
+                if (isntMe(dataRecived.username)) {
+                    const thisPlayersDataEntry = otherPlayers.find((playerData) => {
+                        playerData.username == dataRecived.undefined
+                    })
+
+                    if (thisPlayersDataEntry != "undefined") {
+                        otherPlayers.push(dataRecived)
+                    }
+
+                    const thisPlayerDataEntryIndex = otherPlayers.indexOf(thisPlayersDataEntry)
+                    otherPlayers[thisPlayerDataEntryIndex] = dataRecived
+
+                    console.log("data recived: ", dataRecived)
+                }
+            })
+
+
+            startButton.onclick = () => {
+                gamePage.style.display = "block"
+                landingPage.remove()
+                startGame()
+            }
+        })
+    })
+
 }
-
-// holler.onLoad(() => {
-//     holler.me(() => {
-//         holler.appInstance.notifyClients("hi cli")
-
-//         holler.onClientEvent((event) => {
-//             console.log("Recived: " + event)
-//         })
-//     })
-// })};
